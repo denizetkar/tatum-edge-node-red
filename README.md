33db7b2a5a2beeb82d8eb4b36d90dfd98294d643
Deniz Etkar <denizetkar@gmail.com>
2021-06-30 16:23:29 +0200

    iot edge deployment script has more comments and now it must be run as root; updated README.md

--- README.md
+++ README.md
@@ -5,7 +5,47 @@ This project implements a docker image that contains **Node RED** which was mean
 * However, this docker image was not meant for interactive use via web interface because any changes you make in the deployed container will be deleted when the container is deleted.
 * In order to make changes in the docker image, change contents of the `data` folder before building the image and deploying it.
 
+## Dependencies
+* To build and push the container image:
+  * Latest Docker (>= 20.10.6), see [HERE](https://docs.docker.com/get-docker/) for details.
+  * Latest Powershell (>=7.1.3), see [HERE](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.1) for details.
+* To deploy Azure IoT edge runtime into the IoT device:
+  * A linux device, see [HERE](https://docs.microsoft.com/en-us/azure/iot-edge/how-to-install-iot-edge?view=iotedge-2018-06#prerequisites) for detailed prerequisites.
+  * A `bash` shell,
+  * Internet connection,
+  * Root permissions,
+  * `python3` must be installed (Python 3.7+),
+  * Azure CLI, see [HERE](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli) for more details.
+
 ## Usage
-* Install latest version of **Docker**,
-* Run `build-and-push-image.ps1` with its command line arguments to build the docker image and push it to an image repository to which you must be logged in beforehand using `docker login <image-repo-url>`,
-* Using **Azure Portal** or your own deployment manifest, deploy the image as an edge module to the IoT edge device(s).
+* To build and push the container image:
+  * Open a **powershell** terminal in the project root folder,
+  * Run `build-and-push-image.ps1` with its command line arguments to build the docker image and push it to an image repository to which you must be logged in beforehand using `docker login <image-repo-url>`.
+* To deploy Azure IoT edge runtime into the IoT device:
+  * Open a **bash** terminal in `scripts` folder or `cd scripts` into the folder,
+  * Run `sudo ./deploy-iot-edge.sh`,
+  * In `config.yaml` file, uncomment the following lines:
+    ```yaml
+    provisioning:
+    source: "manual"
+    device_connection_string: ""
+    dynamic_reprovisioning: false
+    ```
+  * Paste the device connection string of the edge device to the corresponding field: `device_connection_string`,
+  * Manually run the paragraph of commands that follow the last uncommented command, in `deploy-iot-edge.sh` at line 48, 49 and 51 in order to:
+    * Copy `config.yaml` back to where it belongs,
+    * Change the owner of the file to `iotedge` user and group,
+    * Restart the `iotedge` linux service to load the configuration.
+  * See [HERE](https://docs.microsoft.com/en-us/azure/iot-edge/how-to-install-iot-edge?view=iotedge-2018-06) for more details.
+* To deploy the image into an IoT edge device:
+  * In the file `manifests/deployment.json`,
+    * Replace `PRIVATE_IMAGE_REGISTRY_HOST` with the image registry url,
+    * Replace `PRIVATE_IMAGE_REGISTRY_USERNAME` with the username,
+    * Replace `PRIVATE_IMAGE_REGISTRY_PASSWORD` with the password.
+  * Open a terminal in the project root folder,
+  * Run `az account list -o table` to check if you are already logged into your Azure account. If a warning appears, run `az login` to login,
+  * Run `az iot edge set-modules --device-id [device id] --hub-name [hub name] --content [file path]` where:
+    * `[device id]` is the device id of the edge device,
+    * `[hub name]` is the name of the Azure IoTHub service,
+    * `[file path]` is the relative path to the deployment manifest json file such as `./manifests/deployment.json`.
+    * See [HERE](https://docs.microsoft.com/en-us/azure/iot-edge/how-to-deploy-modules-cli?view=iotedge-2018-06) for more details.
--- scripts/deploy-iot-edge.sh
+++ scripts/deploy-iot-edge.sh
@@ -1,55 +1,65 @@
 #!/bin/bash
 
-curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list
-sudo mv ./microsoft-prod.list /etc/apt/sources.list.d/
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv ./microsoft.gpg /etc/apt/trusted.gpg.d/
 
-sudo apt-get update
-dpkg-query -l moby-engine &> /dev/null
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+
+# Install 'curl' command.
+apt-get update && sudo apt-get upgrade
+apt-get install -y curl
+
+
+# Update apt repository sources to install 'moby-engine' from Microsoft
+sudo -u $real_user curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list
+mv ./microsoft-prod.list /etc/apt/sources.list.d/
+sudo -u $real_user curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv ./microsoft.gpg /etc/apt/trusted.gpg.d/
+
+apt-get update
+sudo -u $real_user dpkg-query -l moby-engine &> /dev/null
 retVal=$?
 if [[ retVal -ne 0 ]]; then
-    sudo apt-get install -y moby-engine
+    apt-get install -y moby-engine
 fi
-sudo python3 docker-configure.py
-sudo service docker restart
+# Configure container runtime settings to disable infinite logging
+python3 docker-configure.py
+systemctl restart docker.service
+
 
 # Create and import a self-signed certificate to later install it into IoT edge security daemon
-sudo apt-get install -y libssl-dev ca-certificates ntp
-openssl req -x509 -nodes -newkey rsa:4096 -subj "/CN=iotedge-ca" -keyout iotedge-cakey.pem -out iotedge-cacert.pem -days 365000
-sudo mkdir /usr/local/share/ca-certificates/extra
-sudo cp iotedge-cacert.pem /usr/local/share/ca-certificates/extra/iotedge-cacert.crt
-sudo update-ca-certificates
-sudo chmod a+r /var/secrets
-sudo chown iotedge:iotedge /var/secrets/iotedge-cacert.pem /var/secrets/iotedge-cakey.pem
-sudo cp {iotedge-cacert.pem,iotedge-cakey.pem} /var/secrets
-sudo rm iotedge-cacert.pem iotedge-cakey.pem
+apt-get install -y libssl-dev ca-certificates ntp
+sudo -u $real_user openssl req -x509 -nodes -newkey rsa:4096 -subj "/CN=iotedge-ca" -keyout iotedge-cakey.pem -out iotedge-cacert.pem -days 365000
+mkdir /usr/local/share/ca-certificates/extra
+cp iotedge-cacert.pem /usr/local/share/ca-certificates/extra/iotedge-cacert.crt
+update-ca-certificates
+chmod a+r /var/secrets
+chown iotedge:iotedge /var/secrets/iotedge-cacert.pem /var/secrets/iotedge-cakey.pem
+cp {iotedge-cacert.pem,iotedge-cakey.pem} /var/secrets
+rm iotedge-cacert.pem iotedge-cakey.pem
 
-sudo rm -rf /var/lib/iotedge/hsm/certs/* /var/lib/iotedge/hsm/cert_keys/*
-sudo apt-get install -y iotedge
-sudo cp /etc/iotedge/config.yaml ./config.yaml
-sudo chmod u+w ./config.yaml
-sudo chown $(id -un):$(id -gn) ./config.yaml
+
+# Install 'iotedge' apt package.
+rm -rf /var/lib/iotedge/hsm/certs/* /var/lib/iotedge/hsm/cert_keys/*
+apt-get install -y iotedge
+cp /etc/iotedge/config.yaml ./config.yaml
+chmod u+w ./config.yaml
+chown $(id -un):$(id -gn) ./config.yaml
 # After editing config.yaml, move it back to '/etc/iotedge/config.yaml'
 #sudo cp ./config.yaml /etc/iotedge/config.yaml
 #sudo chown iotedge:iotedge /etc/iotedge/config.yaml
 # Then apply the changes
 #sudo systemctl restart iotedge
 
-# sudo apt-get install -y aziot-edge=1.2* aziot-identity-service=1.2*
-# sudo cp /etc/aziot/config.toml.edge.template ./config.toml
-# sudo chown $(id -un):$(id -gn) ./config.toml
-# # After editing config.toml, move it to '/etc/aziot/config.toml'
-# #sudo cp ./config.toml /etc/aziot/config.toml
-# #sudo chown root:root /etc/aziot/config.toml
-# # Then apply the changes
-# #sudo iotedge config apply
-
 
 # To delete everything:
 #sudo apt-get remove --purge -y iotedge
 #sudo rm -rf /run/iotedge /etc/logrotate.d/iotedge /etc/init.d/iotedge /etc/iotedge /etc/default/iotedge /var/lib/iotedge
-
-#sudo apt-get remove --purge -y aziot-edge aziot-identity-service
-#sudo find / \( -path /mnt/c -o -path /mnt/d \) -prune -false -o -iname iotedge -exec rm -rf {} +
-#sudo rm -rf /run/iotedge /etc/logrotate.d/iotedge /etc/init.d/iotedge /etc/iotedge /etc/default/iotedge /var/lib/iotedge /var/secrets/aziot
